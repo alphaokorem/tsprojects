1c92103dbf28e054fce0ef9a6ec0a0529f664649
Abdul Hadi Faisal <159718074+alphaokorem@users.noreply.github.com>
2024-06-15 14:55:41 +0500

    Add files via upload

---
+++ calculator/calculator.js
@@ -0,0 +1,26 @@
+import inquirer from "inquirer";
+const a = await inquirer.prompt([
+    { message: "Enter First Number: ", type: "number", name: "FirstNumber" },
+    { message: "Enter Second Number: ", type: "number", name: "SecondNumber" },
+    {
+        message: "Select Operation: ",
+        type: "list",
+        name: "Operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+if (a.Operator === "Addition") {
+    console.log(a.FirstNumber + a.SecondNumber);
+}
+else if (a.Operator === "Subtraction") {
+    console.log(a.FirstNumber - a.SecondNumber);
+}
+else if (a.Operator === "Multiplication") {
+    console.log(a.FirstNumber * a.SecondNumber);
+}
+else if (a.Operator === "Division") {
+    console.log(a.FirstNumber / a.SecondNumber);
+}
+else {
+    console.log("Give valid values.");
+}
